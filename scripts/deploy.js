9b5b236c113765c159c6687d7d615bd70ce8a1d4
obddd <obaidulhaq20@gmail.com>
2021-10-27 21:18:59 +0500

    created functions & deployed to rinkeby

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,24 +1,32 @@
 const main = async () => {
   const gameContractFactory = await hre.ethers.getContractFactory("MyEpicGame");
   const gameContract = await gameContractFactory.deploy(
-    ["Leo", "Aang", "Pikachu"], // Names
+    ["Leo", "Aang", "Pikachu"],
     [
-      "https://i.imgur.com/pKd5Sdk.png", // Images
+      "https://i.imgur.com/pKd5Sdk.png",
       "https://i.imgur.com/xVu4vFL.png",
-      "https://i.imgur.com/WMB6g9u.png",
+      "https://i.imgur.com/u7T87A6.png",
     ],
-    [100, 200, 300], // HP values
-    [100, 50, 25] // Attack damage values
+    [100, 200, 300],
+    [100, 50, 25]
   );
   await gameContract.deployed();
-  console.log("The contract deployed to:", gameContract.address);
+  console.log("Contract deployed to:", gameContract.address);
 
   let txn;
+  txn = await gameContract.mintCharacterNFT(0);
+  await txn.wait();
+  console.log("Minted NFT #1");
+
+  txn = await gameContract.mintCharacterNFT(1);
+  await txn.wait();
+  console.log("Minted NFT #2");
+
   txn = await gameContract.mintCharacterNFT(2);
   await txn.wait();
+  console.log("Minted NFT #3");
 
-  let returnedTokenUri = await gameContract.tokenURI(1);
-  console.log("Token URI:", returnedTokenUri);
+  console.log("Done deploying and minting!");
 };
 
 const runMain = async () => {
